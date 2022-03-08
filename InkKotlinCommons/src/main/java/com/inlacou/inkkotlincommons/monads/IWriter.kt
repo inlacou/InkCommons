f24bd27ead9c60c84ec81d89fddc464c4d8e34a0
Ilacoume <ilacoume@irontec.com>
2022-03-08 12:45:48 +0100

    Added Writer Monad as an interface

---
+++ InkKotlinCommons/src/main/java/com/inlacou/inkkotlincommons/monads/IWriter.kt
@@ -0,0 +1,12 @@
+package com.inlacou.inkkotlincommons.monads
+interface IWriter {
+    var log: String
+    val iWriterValue: Any
+}
+
+fun <T: IWriter> T.bind(f: (T) -> T): T {
+    val current = this.log
+    return f(this).apply { log = current + log }
+}
+fun <T: IWriter> T.append(s: String): T { log = "$s: $iWriterValue\n"; return this }
+fun <T: IWriter> T.addWriterStep(s: String): T = append(s)
--- InkKotlinCommons/src/main/java/com/inlacou/inkkotlincommons/monads/Writer.kt
+++ InkKotlinCommons/src/main/java/com/inlacou/inkkotlincommons/monads/Writer.kt
@@ -1,7 +1,7 @@
 package com.inlacou.inkkotlincommons.monads
 
 class Writer<T>(val value: T, s: String) {
-    var log = "  ${s.padEnd(17)}: $value\n"
-        private set
-    fun bind(f: (T) -> Writer<T>): Writer<T> = f(this.value).apply { log = this@Writer.log + this.log }
+    var log = "$s: $value\n"
+    fun bind(f: (T) -> Writer<T>): Writer<T> = f(this.value).apply {
+        log = this@Writer.log + this.log }
 }
