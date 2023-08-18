cd87bc9d758c3838c6ad23c87add4f882051eb91
Iñigo Lacoume <inigo.lacoume@picsart.com>
2023-08-18 21:44:04 +0200

    reopen project

--- InkAndroidExtensions/src/main/java/com/inlacou/inkandroidextensions/RxExtensions.kt
+++ InkAndroidExtensions/src/main/java/com/inlacou/inkandroidextensions/RxExtensions.kt
@@ -6,18 +6,18 @@ import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-fun <T> Observable<T>.onUi(): Observable<T> = subscribeOn(AndroidSchedulers.mainThread())
-fun <T> Single<T>.onUi(): Single<T> = subscribeOn(AndroidSchedulers.mainThread())
+fun <T: Any> Observable<T>.onUi(): Observable<T> = subscribeOn(AndroidSchedulers.mainThread())
+fun <T: Any> Single<T>.onUi(): Single<T> = subscribeOn(AndroidSchedulers.mainThread())
 fun Completable.onUi(): Completable = subscribeOn(AndroidSchedulers.mainThread())
 
-fun <T> Observable<T>.toUi(): Observable<T> = observeOn(AndroidSchedulers.mainThread())
-fun <T> Single<T>.toUi(): Single<T> = observeOn(AndroidSchedulers.mainThread())
+fun <T: Any> Observable<T>.toUi(): Observable<T> = observeOn(AndroidSchedulers.mainThread())
+fun <T: Any> Single<T>.toUi(): Single<T> = observeOn(AndroidSchedulers.mainThread())
 fun Completable.toUi(): Completable = observeOn(AndroidSchedulers.mainThread())
 
-fun <T> Observable<T>.onComputation(): Observable<T> = subscribeOn(Schedulers.computation())
-fun <T> Single<T>.onComputation(): Single<T> = subscribeOn(Schedulers.computation())
+fun <T: Any> Observable<T>.onComputation(): Observable<T> = subscribeOn(Schedulers.computation())
+fun <T: Any> Single<T>.onComputation(): Single<T> = subscribeOn(Schedulers.computation())
 fun Completable.onComputation(): Completable = subscribeOn(Schedulers.computation())
 
-fun <T> Observable<T>.toComputation(): Observable<T> = observeOn(Schedulers.computation())
-fun <T> Single<T>.toComputation(): Single<T> = observeOn(Schedulers.computation())
+fun <T: Any> Observable<T>.toComputation(): Observable<T> = observeOn(Schedulers.computation())
+fun <T: Any> Single<T>.toComputation(): Single<T> = observeOn(Schedulers.computation())
 fun Completable.toComputation(): Completable = observeOn(Schedulers.computation())
