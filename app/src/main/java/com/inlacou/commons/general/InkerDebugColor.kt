785a44c69c4e9f7019669e1bfb942e9381db5b58
Inigo Lacoume <inigo.lacoume@picsart.com>
2025-10-14 11:18:58 +0200

    update inker

--- app/src/main/java/com/inlacou/commons/general/InkerDebugColor.kt
+++ app/src/main/java/com/inlacou/commons/general/InkerDebugColor.kt
@@ -7,12 +7,12 @@ import com.inlacou.inker.Inker
  * https://github.com/inlacou/Inker
  */
 class InkerDebugColor: Inker.Color() {
-	override val v: ((s: String?) -> Unit) =       { Log.v(  try{ createTag() }catch (e: Exception) { "INKER_ERROR" }, it ?: "null") }
-	override val d: ((s: String?) -> Unit) =       { Log.d(  try{ createTag() }catch (e: Exception) { "INKER_ERROR" }, it ?: "null") }
-	override val i: ((s: String?) -> Unit) =       { Log.i(  try{ createTag() }catch (e: Exception) { "INKER_ERROR" }, it ?: "null") }
-	override val w: ((s: String?) -> Unit) =       { Log.w(  try{ createTag() }catch (e: Exception) { "INKER_ERROR" }, it ?: "null") }
-	override val w2: ((t: Throwable?) -> Unit) =   { Log.w(  try{ createTag() }catch (e: Exception) { "INKER_ERROR" }, it) }
-	override val e: ((s: String?) -> Unit) =       { Log.e(  try{ createTag() }catch (e: Exception) { "INKER_ERROR" }, it ?: "null") }
-	override val e2: ((t: Throwable?) -> Unit) =   { Log.e(  try{ createTag() }catch (e: Exception) { "INKER_ERROR" }, null, it) }
-	override val wtf: ((s: String?) -> Unit) =     { Log.wtf(try{ createTag() }catch (e: Exception) { "INKER_ERROR" }, it) }
+	override val v: ((s: String?) -> Unit) =       { Log.v(  try { "ink-"+createTag() }catch (e: Exception) { "INKER_ERROR" }, it ?: "null") }
+	override val d: ((s: String?) -> Unit) =       { Log.d(  try { "ink-"+createTag() }catch (e: Exception) { "INKER_ERROR" }, it ?: "null") }
+	override val i: ((s: String?) -> Unit) =       { Log.i(  try { "ink-"+createTag() }catch (e: Exception) { "INKER_ERROR" }, it ?: "null") }
+	override val w: ((s: String?) -> Unit) =       { Log.w(  try { "ink-"+createTag() }catch (e: Exception) { "INKER_ERROR" }, it ?: "null") }
+	override val w2: ((t: Throwable?) -> Unit) =   { Log.w(  try { "ink-"+createTag() }catch (e: Exception) { "INKER_ERROR" }, it) }
+	override val e: ((s: String?) -> Unit) =       { Log.e(  try { "ink-"+createTag() }catch (e: Exception) { "INKER_ERROR" }, it ?: "null") }
+	override val e2: ((t: Throwable?) -> Unit) =   { Log.e(  try { "ink-"+createTag() }catch (e: Exception) { "INKER_ERROR" }, null, it) }
+	override val wtf: ((s: String?) -> Unit) =     { Log.wtf(try { "ink-"+createTag() }catch (e: Exception) { "INKER_ERROR" }, it) }
 }
